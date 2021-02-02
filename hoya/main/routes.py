27866aa926afce691009c23bedfbfce3031834d1
jsm-txt <hose153@gmail.com>
2021-02-02 15:42:21 -0600

    Stuff

--- hoya/main/routes.py
+++ hoya/main/routes.py
@@ -1,6 +1,6 @@
 """Import packages from Flask for our routes."""
 from flask import Blueprint, render_template
-
+from hoya import db
 # This is weird flask syntax that intializes our blueprint
 # You'll notice our routes below look like:
 # main.route('/') instead of what you're used to (app.route('/'))
@@ -12,13 +12,8 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def landingPage():
     """Return our landing page to the user."""
-<<<<<<< HEAD
     # Right now, index.html is an empty file that tells us our server is running
 
     test_listing =db.listings.insert_one({'name':"flower"})
 
-=======
-    # Right now, index.html is an empty file that tells
-    # us our server is running
->>>>>>> 60a31c1587004a0335b6e61810d1ffe354617a22
     return render_template("index.html")
