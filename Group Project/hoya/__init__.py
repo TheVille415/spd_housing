1f8b1bb37229276110c35cabdd26b1ad02ca81d6
Jordan_Torres <jt.arontorres@gmail.com>
2021-02-19 18:11:01 -0800

    rolling back

---
+++ Group Project/hoya/__init__.py
@@ -0,0 +1,19 @@
+"""This is the file where we initialize our application."""
+from flask import Flask
+from hoya.config import Config
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+mongo = PyMongo(app)
+db = mongo.db
+# Visual studio code comes with a linter that might try to
+# push this line to the top of the file. Flask is weird,
+# and this won't work. Make sure it stays here, and if you're getting errors
+# running it, let me know
+from hoya.main.routes import main
+
+# This line "registers" our main blueprint, so flask knows what to execute
+# when we access localhost
+app.register_blueprint(main)
---
+++ Group Project/hoya/main/routes.py
@@ -0,0 +1,83 @@
+"""Import packages from Flask for our routes."""
+
+from flask import Blueprint, render_template, request, redirect, url_for
+from hoya import db
+
+# from bson.objectid import ObjectId
+
+# This is weird flask syntax that intializes our blueprint
+# You'll notice our routes below look like:
+# main.route('/') instead of what you're used to (app.route('/'))
+# We're basically just sectioning off related routes from our app,
+# which will help us preserve readability and maintainability as we scale.
+main = Blueprint("main", __name__)
+
+
+@main.route("/")
+def landingPage():
+    """Return our landing page to the user."""
+
+    # Right now, index.html is an empty file that tells
+    # us our server is running
+    listings = db.listings.find()
+    print(list(listings))
+
+    # Right now, index.html is an empty file that tells us our server is running
+    return render_template("index.html")
+
+
+# TODO: move listing routes to their own blueprint
+
+
+@main.route("/newListing", methods=["POST"])
+def newListing():
+    """Add new listing resource to database."""
+    print(request.form.get("numBedrooms"))
+    newListing = {
+        "numBedrooms": request.form.get("numBedrooms"),
+        "sqFootage": request.form.get("sqFootage"),
+        "numBathrooms": request.form.get("numBathrooms"),
+        "address": {
+            "street": request.form.get("street"),
+            "city": request.form.get("city"),
+            "zip": request.form.get("zip"),
+        },
+    }
+    db.listings.insert_one(newListing)
+    # Remove print statements after testing
+    print(f"Inserted successfully! {newListing}")
+    return redirect(url_for("main.newListing"))
+
+
+@main.route("/updateListing/<id>", methods=["POST"])
+def updateListing(id):
+    """Update existing listing."""
+    updatedListing = db.listings.update_one(
+        {"_id": id},
+        {
+            "$set": {
+                "numBedrooms": request.form.get("numBedrooms"),
+                "sqFootage": request.form.get("sqFootage"),
+                "numBathrooms": request.form.get("numBathrooms"),
+                "address": {
+                    "street": request.form.get("street"),
+                    "city": request.form.get("city"),
+                    "zip": request.form.get("zip"),
+                },
+            }
+        },
+    )
+    # Remove print statements after testing
+    print(updatedListing)
+    # In the future it would be nice if we could display the listing
+    # details by id, and show the updated one after the user changes it
+    return redirect(url_for("main.landingPage"))
+
+
+@main.route("/deleteListing/<id>", methods=["POST"])
+def deleteListing(id):
+    """Delete existing listing by id."""
+    db.listings.delete_one({"_id": id})
+    # Remove print statements after testing
+    print("Deleted successfully!")
+    return redirect(url_for("main.landingPage"))
