c4e562e4561d32fc5ad703414a965e7164197730
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-02-26 08:51:47 -0800

    Included comments for BE in /listings, updated /predict/:id route to add prediction to listing document in database as 'price' attribute.

--- hoya/main/routes.py
+++ hoya/main/routes.py
@@ -31,41 +31,59 @@ def aboutPage():
     return render_template("about.html")
 
 
-@main.route("/listings")
+@main.route("/listings", methods=["GET", "POST"])
 def listingsPage():
     """Display listings by location to user."""
     try:
         # Retrieve listings from Hoya database
         # TODO: query listings by city based on "search form"
+        # (access w request.form.get())
+        # TODO: expect from front-end: "city" and "stateCode"
+        # Put in place of "city" and "state_code" in querystring
+        # currently initializing to none so I don't get errors
+        city = None
+        stateCode = None
+
+        # initialize listings list
         listings = []
-        listings.append(db.listings.find())
+        # find our listings from our database based on the city we get
+        # from the search form
+        listings.append(db.listings.find({"address": {"city": city}}))
+
         # Retrieve listings from external (realtor) API
         url = os.getenv("API_URL")
 
-        # TODO: make city dynamic based on "search form"
         querystring = {
             "city": "New York City",
             "limit": "50",
             "offset": "0",
             "state_code": "NY",
-            "sort": "relevance"
+            "sort": "relevance",
         }
 
         headers = {
-            'x-rapidapi-key': os.getenv("API_KEY"),
-            'x-rapidapi-host': os.getenv("API_HOST")
+            "x-rapidapi-key": os.getenv("API_KEY"),
+            "x-rapidapi-host": os.getenv("API_HOST"),
         }
-        response = requests.request("GET", url, headers=headers, params=querystring).json()
+        response = requests.request(
+            "GET", url, headers=headers, params=querystring
+        ).json()
 
         # For each listing in response["properties"] (excluding metadata)
         # append to our listings list
+        # given that our API returns inconsistent data, handle cases where keys
+        # don't exist
 
         for prop in response["properties"]:
             sqFootage = None
             if prop.get("lot_size", None) is None:
-                sqFootage = prop.get("building_size", {}).get("size", random.randint(900, 3400))
+                sqFootage = prop.get("building_size", {}).get(
+                    "size", random.randint(900, 3400)
+                )
             if prop.get("building_size", None) is None:
-                sqFootage = prop.get("lot_size", {}).get("size", random.randint(900, 3400))
+                sqFootage = prop.get("lot_size", {}).get(
+                    "size", random.randint(900, 3400)
+                )
 
             listing = {
                 "_id": prop.get("property_id", ObjectId()),
@@ -76,15 +94,13 @@ def listingsPage():
                     # TODO: instead of None, pass in city, state from req.form
                     "city": prop.get("address", {}).get("city", None),
                     "state": prop.get("address", {}).get("state", None),
-                    "zip": prop.get("address", {}).get("postal_code", None)
-                }
+                    "zip": prop.get("address", {}).get("postal_code", None),
+                },
             }
             listings.append(listing)
-        # TODO: come up with stock "house" icon for FE to show with each listing
-        # TODO: pass relevent listing data to FE
-        # TODO: check with FE what listings template is called
+        # Pass listings to FE as a list
         return render_template("listings.html", listings=listings)
-    except(KeyError):
+    except (KeyError, ValueError):
         # Return custom 404 error page, set status code to 404
         # We use 404 here (rather than 500) because 404 means
         # "resource not found"
@@ -104,11 +120,15 @@ def result(listingId):
         result = ValuePredictor(sqFootage)  # sq foot here
         # Parse to a string for display.
         prediction = str(result)
+
+        # Add price to our database documents
+        listing["price"] = prediction
+
         # Return our index with our prediction passed in to display
         # TODO: FE team is working on where to display this.
         # Update accordingly.
         return render_template("index.html", prediction=prediction)
-    except(ValueError, TypeError):
+    except (ValueError, TypeError):
         # Return custom 500 error page, set status code to 500
         return render_template("500.html"), 500
 
@@ -125,6 +145,7 @@ def newListing():
             "numBedrooms": request.form.get("numBedrooms"),
             "sqFootage": request.form.get("sqFootage"),
             "numBathrooms": request.form.get("numBathrooms"),
+            "price": None,
             "address": {
                 "street": request.form.get("street"),
                 "city": request.form.get("city"),
@@ -139,7 +160,7 @@ def newListing():
         # Redirect back to landing page
         # TODO: redirect to listings page
         return redirect(url_for("main.landingPage"))
-    except(ValueError, TypeError):
+    except (ValueError, TypeError):
         # Return custom 500 error page, set status code to 500
         return render_template("500.html"), 500
 
@@ -169,7 +190,7 @@ def updateListing(id):
         )
         # Once update completed, redirect user to landing page.
         return redirect(url_for("main.landingPage"))
-    except(ValueError):
+    except (ValueError):
         # Return custom 500 error page, set status code to 500
         return render_template("500.html"), 500
 
@@ -181,6 +202,6 @@ def deleteListing(id):
         # Call delete_one() on listings collection
         db.listings.delete_one({"_id": id})
         return redirect(url_for("main.landingPage"))
-    except(ValueError):
+    except (ValueError):
         # Return custom 500 error page, set status code to 500
         return render_template("500.html"), 500
